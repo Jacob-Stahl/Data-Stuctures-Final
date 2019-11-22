6cca5555de847251a12fba3c8168f95317c5544e
admajors00 <admajors00@gmail.com>
2019-11-22 13:57:16 -0600

    Merge remote-tracking branch 'origin/master' Updated makeMove to be able to roll 3 times # Conflicts: #	doubleLL.h #	main.cpp #	player.h #	roll.h

---
+++ cmake-build-debug/CMakeFiles/Data-Structures-Final.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Data-Structures-Final.dir/main.cpp.obj"
+  "Data-Structures-Final.pdb"
+  "Data-Structures-Final.exe"
+  "Data-Structures-Final.exe.manifest"
+  "libData-Structures-Final.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Data-Structures-Final.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
