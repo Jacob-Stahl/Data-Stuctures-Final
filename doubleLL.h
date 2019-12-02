8fe47fad1cb0dc3e2d6641f5423924e795dfa79e
matthewguthriew <mg10035@yahoo.com>
2019-12-02 23:56:35 +0000

    one shot two shot

--- doubleLL.h
+++ doubleLL.h
@@ -221,65 +221,76 @@ int isDead(Node *player, Node *temp, int pos) {
     }
     return currentDead;
 }
-
-
 void makeMove(Node *player) {
-    Dice die[6];
-    int keepDice[6] = {0, 0, 0, 0, 0, 0};//used in conjunction with the roll function.
-    // index corresponds to dice, 1 to keep 0 to reroll
-    int keep;//when weather or not the dice is to be kept needs to be decided
-    int leftOrRight;//used when shooting players, 1 for left 0 for right
-    int numDynamite = 0; //keeps track of number of dynamite that have been rolled//
-    int numGat = 0; // keeps track of number of gatling gun rolls
-    Node *temp;
+  Dice die[6];
+  int keepDice[6] = {0, 0, 0, 0, 0, 0};//used in conjunction with the roll function.
+  // index corresponds to dice, 1 to keep 0 to reroll
+  int keep;//when weather or not the dice is to be kept needs to be decided
+  int leftOrRight;//used when shooting players, 1 for left 0 for right
+  int numDynamite = 0; //keeps track of number of dynamite that have been rolled//
+  int numGat = 0; // keeps track of number of gatling gun rolls
+  Node *temp;
 
-    for (int j = 0; j < 3; j++) {
-        if (j == 0) {
-            cout << "\t\nRoll Player: " << player->data->number << endl;
-            roll(die);
-            cout << "\nInitial Roll:\n";
-            printDice(die);
-        } else {
-            cout << "\t\nRe-roll Player: " << player->data->number << endl;
-            reroll(die, keepDice);
-            if (j == 2) {
-                cout << "\nFinal Roll:\n";
-                printDice(die);
-            }
-        }
-        for (int i = 0; i < 6; i++) {
-            switch (die[i]) {
-                case arrow:
-                    //cout << "\n Arrow\n";
-                    TOTAL_ARROWS -= 1;
-                    player->data->arrows += 1;
-                    if (TOTAL_ARROWS <= 0) {//when arrows run out have indians attack
-                       // cout << "\n\t\tBETTTTT\n";
-                        temp = player;
-                        int pos = 1;
-                        while (temp->next != player) {
-                            temp->data->health -= temp->data->arrows;
-                            temp->data->arrows = 0;
-                            if (isDead(player, temp, pos)) {//returns 1 only if the current player is dead;
-                                return;
-                            }
-                            temp = temp->next;
-                            pos += 1;
-                        }
-                        TOTAL_ARROWS = 9;
-                    }
-                    break;
-                case dynamite:
-                   // cout << "\n Dynamite\n";
-                    keepDice[i] = 1;
-                    numDynamite++;
-                    if (numDynamite >= 3) {
-                        player->data->health--;
-                        if (isDead(player, player, 0)) {
-                            return;
-                        }
+  for (int j = 0; j < 3; j++) {
+    if (j == 0) {
+      cout << "\t\nRoll Player: " << player->data->number << endl;
+      roll(die);
+      cout << "\nInitial Roll:\n";
+      printDice(die);
+  } else {
+      cout << "\t\nRe-roll Player: " << player->data->number << endl;
+      reroll(die, keepDice);
+      if (j == 2) {
+          cout << "\nFinal Roll:\n";
+          printDice(die);
+      }
+  }
+  for (int i = 0; i < 6; i++) {
+    switch (die[i]) {
+        case arrow:
+            //cout << "\n Arrow\n";
+            TOTAL_ARROWS -= 1;
+            player->data->arrows += 1;
+            if (TOTAL_ARROWS <= 0) {//when arrows run out have indians attack
+                // cout << "\n\t\tBETTTTT\n";
+                temp = player;
+                int pos = 1;
+                while (temp->next != player) {
+                    temp->data->health -= temp->data->arrows;
+                    temp->data->arrows = 0;
+                    if (isDead(player, temp, pos)) {//returns 1 only if the current player is dead;
                         return;
                     }
+                    temp = temp->next;
+                    pos += 1;
+                }
+                TOTAL_ARROWS = 9;
+            }
+            break;
+        case dynamite:
+            // cout << "\n Dynamite\n";
+            keepDice[i] = 1;
+            numDynamite++;
+            if (numDynamite >= 3) {
+                player->data->health--;
+                if (isDead(player, player, 0)) {
+                    return;
+                }
+                return;
+            }
+            break;
+        case oneShot:
+            //cout << "\n oneShot\n";
+            keep = rand() % 2;
+            if (keep) {
+                keepDice[i] = 1;
+                leftOrRight = rand() % 2;
+                if (leftOrRight) {
+                    player->prev->data->health--;
+                    if (isDead(player, player->prev, -1)) {
+                        return;
+                    }
+<<<<<<< HEAD
                     break;
                 case oneShot:
                     //cout << "\n oneShot\n";
@@ -289,83 +300,91 @@ void makeMove(Node *player) {
                         ONEShot(player);
                         if (isDead(player, player->next, 1)) 
                              return;
+=======
+                } else {
+                    player->next->data->health--;
+                    if (isDead(player, player->next, 1)) {
+                        return;
+>>>>>>> origin/master
                     }
-
-                    break;
-                case twoShot:
-                    //cout << "\n twoShot\n";
-                    keep = rand() % 2;
-                    if (keep) {
-                        keepDice[i] = 1;
-                        leftOrRight = rand() % 1;
-                        if (player->next->next != player) {//makes sure the player does not shoot himself
-                            if (leftOrRight) {
-                                player->prev->prev->data->health--;
-                                if (isDead(player, player->prev->prev, -2)) {
-                                    return;
-                                }
-                            } else {
-                                player->next->next->data->health--;
-                                if (isDead(player, player->next->next, 2)) {
-                                    return;
-                                }
-                            }
-                        } else {
-                            if (leftOrRight) {
-                                player->prev->data->health--;
-                                if (isDead(player, player->prev, -1)) {
-                                    return;
-                                }
-                            } else {
-                                player->next->data->health--;
-                                if (isDead(player, player, 0)) {
-                                    return;
-                                }
-                            }
-                        }
-                    }
-                    break;
-                case beer:
-                    //cout << "\n beer\n";
-                    if (player->data->health != player->data->max_h) {//cannot go over max health
-                        keep = rand() % 2;
-                        if (keep) {
-                            keepDice[i] = 1;
-                            player->data->health++;
-                        }
-                    }
-                    break;
-                case gatGun:
-                    //cout << "\n gatGun\n";
-                    keep = rand() % 2;
-                    if (keep) {
-                        keepDice[i] = 1;
-                        numGat++;
-                        TOTAL_ARROWS += player->data->arrows; //add arrows back to pile
-                        player->data->arrows = 0;
-
-                        if (numGat == 3) {
-                            temp = player;
-                            temp = temp->next;
-                            int pos = 1;
-                            while (temp != player) {//iterate through players and subtract a life point from each
-                                temp->data->health--;
-                                if (isDead(player, temp, pos)) {
-                                    return;
-                                }
-                                temp = temp->next;
-                                pos++;
-                            }
-                        }
-                    }
-                    break;
-                default:
-                    cout << "ROLL ERROR\n";
-                    break;
-
+                }
             }
-        }
+
+            break;
+        case twoShot:
+            //cout << "\n twoShot\n";
+            keep = rand() % 2;
+            if (keep) {
+                keepDice[i] = 1;
+                leftOrRight = rand() % 1;
+                if (player->next->next != player) {//makes sure the player does not shoot himself
+                    if (leftOrRight) {
+                        player->prev->prev->data->health--;
+                        if (isDead(player, player->prev->prev, -2)) {
+                            return;
+                        }
+                    } else {
+                        player->next->next->data->health--;
+                        if (isDead(player, player->next->next, 2)) {
+                            return;
+                        }
+                    }
+                } else {
+                    if (leftOrRight) {
+                        player->prev->data->health--;
+                        if (isDead(player, player->prev, -1)) {
+                            return;
+                        }
+                    } else {
+                        player->next->data->health--;
+                        if (isDead(player, player, 0)) {
+                            return;
+                        }
+                    }
+                }
+            }
+            break;
+        case beer:
+            //cout << "\n beer\n";
+            if (player->data->health != player->data->max_h) {//cannot go over max health
+                keep = rand() % 2;
+                if (keep) {
+                    keepDice[i] = 1;
+                    player->data->health++;
+                }
+            }
+            break;
+        case gatGun:
+            //cout << "\n gatGun\n";
+            keep = rand() % 2;
+            if (keep) {
+                keepDice[i] = 1;
+                numGat++;
+                TOTAL_ARROWS += player->data->arrows; //add arrows back to pile
+                player->data->arrows = 0;
+
+                if (numGat == 3) {
+                    temp = player;
+                    temp = temp->next;
+                    int pos = 1;
+                    while (temp != player) {//iterate through players and subtract a life point from each
+                        temp->data->health--;
+                        if (isDead(player, temp, pos)) {
+                            return;
+                        }
+                        temp = temp->next;
+                        pos++;
+                    }
+                }
+            }
+            break;
+        default:
+            cout << "ROLL ERROR\n";
+            break;
+
+      }
     }
+  }
 }
 
 
